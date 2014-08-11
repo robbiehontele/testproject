2bd9c75511c4454419d981ad7f3b917512b80ba9
robbiehontele <robbie-hontele@pelmac003.pelckmans.be>
2014-08-11 09:23:29 +0200

    Footer update

--- footer.php
+++ footer.php
@@ -4,5 +4,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <?php wp_footer(); ?>
+    <!-- Deze footer is nu voorzien van testcommentaar-->
   </body>
 </html>
